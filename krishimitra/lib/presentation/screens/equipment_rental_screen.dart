602c6f09432df8cce6b45f7e041a4f492aa70aaa
“[firstname <raj.232313105@vcet.edu.in>
2026-02-27 18:08:00 +0530

    feat: Implement equipment rental marketplace
    - Built equipment listing interface
    - Added rental booking system
    - Created equipment search and filter

---
+++ krishimitra/lib/presentation/screens/equipment_rental_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EquipmentRentalScreen extends StatelessWidget {
+  const EquipmentRentalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Equipment Rental')),
+      body: const Center(child: Text('Coming Soon')),
+    );
+  }
+}
